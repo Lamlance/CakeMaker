dfb86b10bb8132a2ac3b1122ab2c200e12dc92ba
Han Phu <phuhanld@gmail.com>
2021-11-13 09:41:28 +0700

    Cononut Cake

---
+++ CakeMaker/Cake_Coconut.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef CAKE_COCONUT_H
+#define CAKE_COCONUT_H
+#include "Cake.h"
+
+class CakeCoconut :public CakeLayer
+{
+public:
+	std::string desc() { return "Coconut Cake Layer"; };
+	int price() { return 25000; }
+	double timeTake() { return 5; };
+};
+
+
+#endif // !CAKE_CORN_H
