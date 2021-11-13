173ac9e5fc9b1d609693ad7ccefcb5a0f904e6aa
Han Phu <phuhanld@gmail.com>
2021-11-13 09:53:18 +0700

    Vanilla Cake

---
+++ CakeMaker/Cake_Vanilla.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef CAKE_VANILLA_H
+#define CAKE_VANILLA_H
+#include "Cake.h"
+
+class CakeVanilla :public CakeLayer
+{
+public:
+	std::string desc() { return "Vanilla Cake Layer"; };
+	int price() { return 45000; }
+	double timeTake() { return 10; };
+};
+
+
+#endif // !CAKE_CORN_H
