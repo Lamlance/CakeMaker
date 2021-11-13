3607a2af48b009fea2326eedec8a84a82493419a
Han Phu <phuhanld@gmail.com>
2021-11-13 10:06:45 +0700

    Add Caramel

--- CakeMaker/AddCaramel.h
+++ CakeMaker/AddCaramel.h
@@ -3,13 +3,13 @@
 #define CAKEDECO_CARAMEL_H
 #include "CakeLayer_Decorator.h"
 
-class addCheese : public CakeLayer_Decorator
+class addCaramel : public CakeLayer_Decorator
 {
 public:
-	addCheese(CakeLayer* obj, int amountVal = 1) :CakeLayer_Decorator(obj), amount(amountVal) {};
-	std::string desc() { return CakeLayer_Decorator::desc() + " + Cheese x " + std::to_string(amount); };
-	int price() { return CakeLayer_Decorator::price() + (amount * 5000); };
-	double timeTake() { return CakeLayer_Decorator::timeTake() + (amount * 0.2); };
+	addCaramel(CakeLayer* obj, int amountVal = 1) :CakeLayer_Decorator(obj), amount(amountVal) {};
+	std::string desc() { return CakeLayer_Decorator::desc() + " + Caramel x " + std::to_string(amount); };
+	int price() { return CakeLayer_Decorator::price() + (amount * 10000); };
+	double timeTake() { return CakeLayer_Decorator::timeTake() + (amount * 0.5); };
 private:
 	int amount;
 };
