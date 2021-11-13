c0b32717c125984542227c216bc7a6bd1eb2eb3b
Han Phu <phuhanld@gmail.com>
2021-11-13 10:11:26 +0700

    add Apple

---
+++ CakeMaker/addApple.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef CAKEDECO_APPLE_H
+#define CAKEDECO_APPLE_H
+#include "CakeLayer_Decorator.h"
+
+class addApple : public CakeLayer_Decorator
+{
+public:
+	addApple(CakeLayer* obj, int amountVal = 1) :CakeLayer_Decorator(obj), amount(amountVal) {};
+	std::string desc() { return CakeLayer_Decorator::desc() + " + Caramel x " + std::to_string(amount); };
+	int price() { return CakeLayer_Decorator::price() + (amount * 3000); };
+	double timeTake() { return CakeLayer_Decorator::timeTake() + (amount * 0.1); };
+private:
+	int amount;
+};
+
+
+#endif // CAKEDECO_APPLE_H
